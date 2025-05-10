0409c35da5990ebe3b7011063363ef683c109f0e
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-05-11 06:07:02 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -3,3 +3,30 @@ game.StarterGui:SetCore("SendNotification", {
     Text = "Loading",
     Duration = 5
 })
+local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source.lua"))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "W-Azure [Custom]",
+    LoadingTitle = "Đang tải...",
+    LoadingSubtitle = "Delta Executor",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "WAzureCustom",
+        FileName = "Settings"
+    },
+    Discord = {Enabled = false},
+    KeySystem = false
+})
+
+local Tabs = {
+    Main = Window:CreateTab("Main Farm", 4483362458),
+    Stack = Window:CreateTab("Stack Auto Farm", 4483362458),
+    Sub = Window:CreateTab("Sub Farming", 4483362458),
+    Volcano = Window:CreateTab("Volcanic", 4483362458),
+    Status = Window:CreateTab("Status", 4483362458),
+    Player = Window:CreateTab("Player Status", 4483362458),
+    Fruit = Window:CreateTab("Fruit", 4483362458),
+    Local = Window:CreateTab("Local Player", 4483362458)
+}
+
+Rayfield:LoadConfiguration()
