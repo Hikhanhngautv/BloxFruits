e91ef756ed43fc1ecc610ba3573271cc84e6d7ca
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-05-11 06:26:08 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -3,32 +3,67 @@ game.StarterGui:SetCore("SendNotification", {
     Text = "Loading",
     Duration = 5
 })
-# Menu script hack
+-- Simple interactive console menu in Lua
 
-import os
+local function clearScreen()
+  -- Clear the terminal screen
+  if package.config:sub(1,1) == '\\' then
+    os.execute("cls") -- Windows
+  else
+    os.execute("clear") -- Unix-based
+  end
+end
 
-def display_menu():
-    print("1. Option 1")
-    print("2. Option 2")
-    print("3. Option 3")
-    print("4. Exit")
+local function menu()
+  print("===================================")
+  print("        WELCOME TO LUA MENU        ")
+  print("===================================")
+  print("1. Say Hello")
+  print("2. Show Lua version")
+  print("3. List files in current directory")
+  print("4. Exit")
+  print("===================================")
+  io.write("Enter your choice (1-4): ")
+end
 
-def main():
-    while True:
-        display_menu()
-        choice = input("Please select an option: ")
-        
-        if choice == '1':
-            print("You selected Option 1.")
-        elif choice == '2':
-            print("You selected Option 2.")
-        elif choice == '3':
-            print("You selected Option 3.")
-        elif choice == '4':
-            print("Exiting the menu.")
-            break
-        else:
-            print("Invalid choice. Please try again.")
+local function sayHello()
+  print("\nHello! Hope you have a great day!\n")
+end
 
-if __name__ == "__main__":
-    main()
+local function showLuaVersion()
+  print("\nLua version: " .. _VERSION .. "\n")
+end
+
+local function listFiles()
+  print("\nFiles in current directory:")
+  local p = io.popen("ls")
+  for file in p:lines() do
+    print(" - " .. file)
+  end
+  p:close()
+  print()
+end
+
+local function main()
+  while true do
+    clearScreen()
+    menu()
+    local choice = io.read("*line")
+    if choice == "1" then
+      sayHello()
+    elseif choice == "2" then
+      showLuaVersion()
+    elseif choice == "3" then
+      listFiles()
+    elseif choice == "4" then
+      print("\nGoodbye! Exiting the menu.\n")
+      break
+    else
+      print("\nInvalid choice! Please enter a number between 1 and 4.\n")
+    end
+    io.write("Press Enter to continue...")
+    io.read("*line")
+  end
+end
+
+main()
