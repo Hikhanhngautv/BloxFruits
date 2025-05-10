4b49d8d0abd6ae9e152bdb5c20cb1f0113f338a1
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-05-11 06:35:04 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -3,3 +3,10 @@ game.StarterGui:SetCore("SendNotification", {
     Text = "Loading",
     Duration = 5
 })
+if not is_from_loader then warn('Use the loadstring, do not run this directly') return end;
+ 
+do local init_fn = function(...)
+ 
+    script_key = script_key or getgenv().script_key 
+end; 
+init_fn(...); end;
