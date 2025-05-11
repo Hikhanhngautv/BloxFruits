90f746ff9f5424dbee63611d1e85501f79f38000
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-05-11 07:51:19 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -3,30 +3,3 @@ game.StarterGui:SetCore("SendNotification", {
     Text = "Loading",
     Duration = 5
 })
-repeat task.wait() until game:IsLoaded()
-
-local executorname = "Unknown"
-if getexecutorname then
-    executorname = getexecutorname()
-elseif identifyexecutor then
-    executorname = identifyexecutor()
-end
-
-local FAKE_EXECUTOR = { "xeno", "jjsploit" }
-
-for i,v in pairs(FAKE_EXECUTOR) do
-    if executorname:lower():find(v) then
-        game.Players.LocalPlayer:Kick("\n\n" .. executorname .. " is not supported")
-        return
-    end
-end
-
-local GameId = game.GameId
-
-local GameList = {
-    [5682590751] = "Lootify",
-    [994732206] = "Blox%20Fruits/Loader.lua",
-    [1451439645] = "King%20Legacy/Loader.lua",
-    [6765805766] = "Block%20Spin/Loader.lua",
-    [7095682825] = "Beaks/Default.lua"
-}
