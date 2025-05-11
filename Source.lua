406d934eb212f403d1045fe35c27425fa7d209c8
Hikhanhngautv <119961760+Hikhanhngautv@users.noreply.github.com>
2025-05-11 07:27:40 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -3,24 +3,3 @@ game.StarterGui:SetCore("SendNotification", {
     Text = "Loading",
     Duration = 5
 })
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source.lua"))()
-
-local Window = Rayfield:CreateWindow({
-    Name = "W-Azure Custom",
-    LoadingTitle = "Đang tải...",
-    LoadingSubtitle = "Delta Executor",
-    ConfigurationSaving = {
-        Enabled = true,
-        FolderName = "WAzureCustom",
-        FileName = "Settings"
-    },
-    KeySystem = false
-})
-
-local Tab = Window:CreateTab("Main", 4483362458)
-Tab:CreateButton({
-    Name = "Kiểm tra menu",
-    Callback = function()
-        print("Menu hoạt động!")
-    end
-})
